cead9e10fc81449f80e961647e00449116b30e80
Alexander Scheurer <mail@aspepex.net>
2020-11-23 18:03:47 +0100

    Complete DocFX project rebuild

--- Build.cmd
+++ Build.cmd
@@ -1,20 +1,21 @@
 @echo off
 setlocal
 
-SET ROOT=%CD%
 SET OUTPUTPATH=%CD%\docs
-SET BASEURL=https://fuseeprojectteam.github.io/WebsiteTest/
-
-SET FUSEESRC=../Fusee/src
-
 SET DOCFX="%CD%\bin\DocFX\docfx.exe"
-SET HUGO="%CD%\bin\Hugo\hugo.exe"
+SET GHWSTDFX="%CD%\bin\GitHubWikiSidbarToDocFX\GitHubWikiSidbarToDocFX.exe"
 
-%DOCFX% "%CD%\src\DocFX\docfx.json" -o "%OUTPUTPATH%"
+rd /s /q %OUTPUTPATH%
+mkdir %OUTPUTPATH%
 
-rem %HUGO% --destination %OUTPUTPATH% --theme=beautifulhugo --baseURL %BASEURL% --cleanDestinationDir -s .\src\Hugo
+if not x%CD:Test=%==x%CD% goto SkipCNAME
+if not x%CD:New=%==x%CD% goto SkipCNAME
 
+echo fusee3d.org > %OUTPUTPATH%\CNAME
 
+:SkipCNAME
 
+rem %GHWSTDFX% docfx_project\wiki\_Sidebar.md docfx_project\wiki
+%DOCFX% docfx_project\docfx.json
 
 endlocal
